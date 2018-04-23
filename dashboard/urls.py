0c2844bb2a215beb484abc97cf88d2e56bf1b13a
ge jianyu <ge jianyu@detecon.com>
2018-04-23 20:24:43 +0800

    add project

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import *
 from . import views
 from django.conf.urls.static import static
-from dashboard.views import carOwnerChartPage, carOwnerChart, peopleChartPage, peopleChart, my_login, LTPChartPage, LTPChart, \
-    CPChartPage, CPChart, ConfigChartPage, ConfigCompany, ConfigSeriesChart, ConfigModleChart, priceChartPage, PriceModleChart, \
-    PriceCompany, PriceSeries
+from dashboard.views import carOwnerChartPage, carOwnerChart, my_login
 urlpatterns = [
     # index
     url(r'^$', views.index, name='index'),
@@ -15,25 +13,6 @@ urlpatterns = [
     # car dashboard page
     url(r'^auto_page', carOwnerChartPage, name='carEchart'),
     url(r'^auto/$', carOwnerChart, name='carEchartPage'),
-    # people dashboard page
-    url(r'^people_page', peopleChartPage, name='peopleEchart'),
-    url(r'^people/$', peopleChart, name='peopleEchartPage'),
-    # LTP dashborad page
-    url(r'^LTP_page', LTPChartPage, name='LTPChart'),
-    url(r'^LTP/$', LTPChart, name='LTPPage'),
-    # CP dashboard page
-    url(r'^CP_page', CPChartPage, name='CPChart'),
-    url(r'^CP/$', CPChart, name='CPEchartPage'),
-    # Config dashboard page
-    url(r'^Config_page', ConfigChartPage, name='ConfigChart'),
-    url(r'^Config/$', ConfigCompany, name='ConfigCompanyPage'),
-    url(r'^Config_/$', ConfigSeriesChart, name='ConfigSeriesPage'),
-    url(r'^Confi/$', ConfigModleChart, name='ConfigModlePage'),
-    # price dashboard page
-    url(r'^price_page', priceChartPage, name="pricePage"),
-    url(r'^price_company/$', PriceCompany, name='PriceCompanyPage'),
-    url(r'^price_series/$', PriceSeries, name='PriceSeriesPage'),
-    url(r'^price/$', PriceModleChart, name="Price_ModlePage"),
 
 ]
 
